2e65a9f1df86115f5fef62e45dba88bc16d059a2
chris708 <christyfinny@gmail.com>
2025-02-06 18:08:54 +0530

    Added template form for add property

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { FormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent},
@@ -31,6 +32,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,13 +8,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm!: NgForm;
 
   constructor(private route: Router) { }
 
   ngOnInit() {
   }
-
-  onBack() {
-    this.route.navigate(['/']);
-  } 
+  
+  onSubmit(){
+    console.log('Congrats, Form Submitted');
+    console.log(this.addPropertyForm);
+  }
 }
